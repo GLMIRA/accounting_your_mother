55d8d9f30556b23d31dd22bcc44ae4e6553345a5
Fabio Mira <fgmira@gmail.com>
2024-01-29 00:01:38 -0300

    setup inicial dos layouts do sistema

--- aym/aym/settings.py
+++ aym/aym/settings.py
@@ -131,7 +131,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = "staticfiles"
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "aym/static",
+]
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
